0cd1436f4eb38d423ae8e45c85b2047327c80cac
SEVERINE-GARDOUX <severine0613@gmail.com>
2022-02-22 16:32:22 +0100

    katacat

---
+++ kata4.js
@@ -0,0 +1,14 @@
+const cat  = document.querySelector ('a')
+
+cat.addEventListener ('click', function () {
+
+if (cat.innerHTML == '😻') {
+    cat.innerHTML = '😹'}
+else
+cat.innerHTML = '😻'
+})
+
+
+clicaudio.onclick = function () {
+    audio.play()
+}
