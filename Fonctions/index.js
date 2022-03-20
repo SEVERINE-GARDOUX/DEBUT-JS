13e718e2fe324eac7c9ec2cdd7f25846e06c1aa2
SEVERINE-GARDOUX <severine0613pro@gmail.com>
2022-03-20 15:08:31 +0100

    2003

--- Fonctions/index.js
+++ Fonctions/index.js
@@ -11,9 +11,17 @@ let table_multiplication = () => {
     }
 }
 
-function table_multiplication ()  { //on donne des paramètres a note fonction entre les parenthèses
+function table_multiplication ()  { //on donne des paramètres a notre fonction entre les parenthèses
     for (let index = 0; index < 13; index++) {
         console.log (`3 x ${index} = ${index+3}`)
     }
 }
 
+let carre = (nombre) => {
+    if (nombre && typeof (nombre) === "number") {
+        return nombre*nombre
+    }else {
+        return null
+    }
+}
+// tout s'arrête au return. Rien ne sera lu après le return
---
+++ Objets/index.js
@@ -0,0 +1,11 @@
+// Les Objets
+
+let car = new Object ()
+
+//ajout de caractériqtiques a l'objet
+let car2 = {
+_id :"CL222CT",
+name : "Fiat",
+year : 2000,
+model : punto2
+}
