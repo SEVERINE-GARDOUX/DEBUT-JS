b68fa4a2319419ed83b9c269edde5806397008ec
SEVERINE-GARDOUX <severine0613pro@gmail.com>
2022-03-20 17:50:56 +0100

    2003

---
+++ 2emeCours/LesElements/index.js
@@ -0,0 +1,18 @@
+ // Id
+ let html = document.getElementById ('html')
+
+ //class
+//  let section = document.getElementsByClassName ('section') 
+
+ //pour sélectionner un élément
+ section[0].getElementsByTagName ('article')
+
+ document.getElementsByName ('email')
+
+ let input = document.getElementsByTagName ('input')
+
+let body = document.getElementsByTagName ('body')
+
+//par selecteur css
+let section = document.querySelectorAll ('section')
+let css = document.querySelector ('#css')
